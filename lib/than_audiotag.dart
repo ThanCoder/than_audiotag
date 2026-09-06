22ae48a4de73861e3f73a99aacfadc693fac8df3
ThanCoder <thancoder2019@gmail.com>
2026-09-06 15:01:19 +0630

    update

--- lib/than_audiotag.dart
+++ lib/than_audiotag.dart
@@ -6,8 +6,13 @@ export 'core/chatgpt_native/index.dart';
 export 'core/my_native/props/index.dart';
 export 'core/my_native/workers/tag_picture_worker.dart';
 
+final _dylib = DynamicLibrary.open('libtag.so');
+
 ThanAudiotagBindings getTag({String? libPath}) {
-  final dylib = DynamicLibrary.open(libPath ?? 'libtag.so');
+  DynamicLibrary dylib = _dylib;
+  if (libPath != null) {
+    dylib = DynamicLibrary.open(libPath);
+  }
 
   // dev
   // final dylib = DynamicLibrary.open(
